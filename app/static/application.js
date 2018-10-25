5cb90c9921f7ea78645fc099d94651caeb8a0527
Branislav Pecher <branop95@gmail.com>
2018-10-25 17:51:17 +0200

    Update boilerplate

---
+++ app/static/application.js
@@ -0,0 +1,5 @@
+var mymap = L.map('mapid').setView([48.2875, 17.2698], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
